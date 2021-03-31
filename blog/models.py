521a875a6ffc2af61a709553aff104c87485d9a0
dhruv8609 <dhruv.sh2017@gmail.com>
2021-03-31 14:44:14 +0530

    added post functionality

--- blog/models.py
+++ blog/models.py
@@ -1,15 +1,20 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
 # Create your models here.
 
-class Post(models.Model):
-	title = models.CharField(max_length=100)
-	content = models.TextField()
-	author = models.ForeignKey(User , on_delete = models.CASCADE)
-	date_posted = models.DateTimeField(default=timezone.now)
-	#auto_now set the date whenever the post is updated.
-	#auto_now_add allow to have exact date it was first added
 
-	def __str__(self):
-		return self.title; 
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    date_posted = models.DateTimeField(default=timezone.now)
+    # auto_now set the date whenever the post is updated.
+    # auto_now_add allow to have exact date it was first added
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('post-detail', kwargs={'pk': self.pk})
